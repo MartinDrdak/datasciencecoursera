6fe9b5d4532d69f20807bff1ecc9ec2d4a3e8a37
MartinDrdak <martin@drdak.com>
2014-04-26 13:28:22 +0200

    version2 & TidyData

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,31 @@
-# Source of data for the project:
-# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-# This R script does the following:
-# 1. Merges the training and the test sets to create one data set.
+#The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. You will be graded by your peers on a series of yes/no questions related to the project. You will be required to submit: 1) a tidy data set as described below, 2) a link to a Github repository with your script for performing the analysis, and 3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.  
+#One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
+#http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+#Here are the data for the project: 
+#https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+#You should create one R script called run_analysis.R that does the following. 
+#1.Merges the training and the test sets to create one data set.
+#2.Extracts only the measurements on the mean and standard deviation for each measurement. 
+#3.Uses descriptive activity names to name the activities in the data set
+#4.Appropriately labels the data set with descriptive activity names. 
+#5.Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-tmp1 <- read.table("train/X_train.txt")
-tmp2 <- read.table("test/X_test.txt")
-X <- rbind(tmp1, tmp2)
 
-tmp1 <- read.table("train/subject_train.txt")
-tmp2 <- read.table("test/subject_test.txt")
-S <- rbind(tmp1, tmp2)
+#1.Merges the training and the test sets to create one data set
 
-tmp1 <- read.table("train/y_train.txt")
-tmp2 <- read.table("test/y_test.txt")
-Y <- rbind(tmp1, tmp2)
+setwd("UCI HAR Dataset")
+
+temp1 <- read.table("train/X_train.txt")
+temp2 <- read.table("test/X_test.txt")
+X <- rbind(temp1, temp2)
+
+temp1 <- read.table("train/subject_train.txt")
+temp2 <- read.table("test/subject_test.txt")
+S <- rbind(temp1, temp2)
+
+temp1 <- read.table("train/y_train.txt")
+temp2 <- read.table("test/y_test.txt")
+Y <- rbind(temp1, temp2)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
@@ -22,7 +34,7 @@ indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
 X <- X[, indices_of_good_features]
 names(X) <- features[indices_of_good_features, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
-names(X) <- tolower(names(X))  # see last slide of the lecture Editing Text Variables (week 4)
+names(X) <- tolower(names(X))  
 
 # 3. Uses descriptive activity names to name the activities in the data set
 
@@ -30,6 +42,10 @@ activities <- read.table("activity_labels.txt")
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
 Y[,1] = activities[Y[,1], 2]
 names(Y) <- "activity"
+
+# write to root
+setwd("..")
+
 
 # 4. Appropriately labels the data set with descriptive activity names.
 
@@ -57,10 +73,4 @@ for (s in 1:numSubjects) {
 }
 write.table(result, "data_set_with_the_averages.txt")
 
-# res2 <- read.table("data_set_with_the_averages.txt")
-# result[4,4]
-# res2[4,4]
-# res2[4,4]==result[4,4]
-# result[6,4]
-# res2[6,4]
-# res2[6,4]==result[6,4]
+ 
